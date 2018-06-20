9b53e32307f52c5bf46fda42d353cbca2d8cf5b4
patorusty <rrusty@hotmail.com>
2018-06-20 19:53:39 -0300

    2

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
         <div class="row d-flex justify-content-around align-items-center mt-5">
         
-                <a href=""><img src="images/brands/adidas.jpg" class="brand-img mt-3" alt=""></a>
+                    <a href="/brands/6"><img src="images/brands/adidas.jpg" class="brand-img mt-3" alt=""></a>
             
-                    <a href=""><img src="images/brands/newera.jpg" class="brand-img mt-3" alt=""></a>
+                    <a href="brands/3"><img src="images/brands/newera.jpg" class="brand-img mt-3" alt=""></a>
                 
-                    <a href=""><img src="images/brands/nike.jpg" class="brand-img mt-3" alt=""></a>
+                    <a href="brands/3"><img src="images/brands/nike.jpg" class="brand-img mt-3" alt=""></a>
                 
-                    <a href=""><img src="images/brands/flexfit.jpg" class="brand-img mt-3" alt=""></a>
+                    <a href="brands/3"><img src="images/brands/flexfit.jpg" class="brand-img mt-3" alt=""></a>
             </div>
 @endsection
